f95ce68b27e534699af0c90e474949a94b5a3f0d
Mohammad Batous <thesyrianprogrammer@gmail.com>
2024-10-11 16:21:18 +0300

    Add files via upload

---
+++ my-portfolio/app.js
@@ -0,0 +1,10 @@
+/*
+
+    CODING BY MOHAMMAD MAHMOUD BATOUS 
+        I LOVE CODING AND PROGRAMMING
+            I LOVE CYBER SECURITY 
+                I LOVE WRITE CODE EVERY TIME
+                    THANK YOU FOR WATCH 
+                        RESPECT 🫡
+
+*/
